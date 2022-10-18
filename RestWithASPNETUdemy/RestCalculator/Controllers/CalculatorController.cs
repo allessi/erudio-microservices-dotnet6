c409cde7f1a1d1a6ac907495778099287f85ec7c
Allessi <allessi.souza.externo@ageas.pt>
2022-10-17 21:32:08 -0300

    my first branch

---
+++ RestWithASPNETUdemy/RestCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+
+namespace RestCalculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("subtract/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtract(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multiply/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiply(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("divide/{firstNumber}/{secondNumber}")]
+        public IActionResult Divide(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult Average(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("square-root/{firstNumber}")]
+        public IActionResult SquareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var sum = Math.Sqrt(ConvertToDouble(firstNumber));
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private double ConvertToDouble(string number)
+        {
+            _ = double.TryParse(number, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out double result);
+            return result;
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            _ = decimal.TryParse(number, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out decimal result);
+            return result;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            return decimal.TryParse(number, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out _);
+        }
+    }
+}
---
+++ RestWithASPNETUdemy/RestClassControllerAndModel/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,42 @@
+﻿using RestClassControllerAndModel.Model;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestClassControllerAndModel.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int IdPerson;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person() { Id = 1, FirstName = "Person", LastName = "Last Name", Address = "Some Address", Gender = "Male" };
+        }
+
+        public List<Person> GetAll()
+        {
+            var persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Interlocked.Increment(ref IdPerson);
+                var person = new Person() { Id = IdPerson, FirstName = $"Person {IdPerson}", LastName = $"Last Name {IdPerson}", Address = $"Some Address {IdPerson}", Gender = i % 2 == 0 ? "Male" : "Female" };
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
